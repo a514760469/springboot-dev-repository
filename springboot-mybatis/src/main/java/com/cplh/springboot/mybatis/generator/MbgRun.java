a434bc75fcedc4559e171c1dc29af8baf40ea464
zhanglifeng <514760469@qq.com>
2019-07-29 17:22:26 +0800

    mbg生成 还有一些问题

---
+++ springboot-mybatis/src/main/java/com/cplh/springboot/mybatis/generator/MbgRun.java
@@ -0,0 +1,15 @@
+package com.cplh.springboot.mybatis.generator;
+
+public class MbgRun {
+
+    public static void main(String[] args) throws Exception {
+        /*List<String> warnings = new ArrayList<>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(MbgRun.class.getResourceAsStream("classpath:generatorConfig.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);*/
+
+    }
+
+}
---
+++ springboot-mybatis/src/test/java/com/cplh/springboot/mybatis/dao/StudentMapperTest.java
@@ -0,0 +1,18 @@
+package com.cplh.springboot.mybatis.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class StudentMapperTest {
+
+    @Test
+    public void testAdd() {
+
+    }
+
+
+}
