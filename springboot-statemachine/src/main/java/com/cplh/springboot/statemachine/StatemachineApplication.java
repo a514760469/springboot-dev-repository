9c96fac1a886ef6acf261ad32d1983ebb83b6024
zhanglifeng <514760469@qq.com>
2020-06-29 16:39:26 +0800

    spring、statemachine

---
+++ springboot-statemachine/src/main/java/com/cplh/springboot/statemachine/StatemachineApplication.java
@@ -0,0 +1,17 @@
+package com.cplh.springboot.statemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhanglifeng
+ * @since 2020-06-03 14:06
+ */
+@SpringBootApplication
+public class StatemachineApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StatemachineApplication.class, args);
+    }
+}
---
+++ springboot-statemachine/src/main/java/com/cplh/springboot/statemachine/config/States.java
@@ -0,0 +1,13 @@
+package com.cplh.springboot.statemachine.config;
+
+/**
+ * @author zhanglifeng
+ * @since 2020-06-03 14:11
+ */
+public enum States {
+
+    S_INITIAL,// 初始状态
+    S1,
+    S2,
+    S_ENDING // 结束状态
+}
