197f18622954bf5b7238b54c28efe43cbce4d8cd
zhanglifeng <514760469@qq.com>
2020-11-12 17:33:20 +0800

    代码整理

--- springboot-oauth2-authorization-server/src/main/java/com/spring/oauth2/SpringbootOauth2AuthrizationServerApplication.java
+++ springboot-oauth2-authorization-server/src/main/java/com/spring/oauth2/SpringbootOauth2AuthrizationServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootOauth2AuthrizationServerApplication {
+public class SpringbootOauth2AuthorizationServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootOauth2AuthrizationServerApplication.class, args);
+        SpringApplication.run(SpringbootOauth2AuthorizationServerApplication.class, args);
     }
 
 }
--- springboot-oauth2-authorization-server/src/test/java/com/spring/oauth2/SpringbootOauth2AuthorizationServerApplicationTests.java
+++ springboot-oauth2-authorization-server/src/test/java/com/spring/oauth2/SpringbootOauth2AuthorizationServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootOauth2AuthrizationServerApplicationTests {
+public class SpringbootOauth2AuthorizationServerApplicationTests {
 
     @Test
     public void contextLoads() {
